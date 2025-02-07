d2cbf08ab0bb583a37f7f4b4fcf579233c9735c0
Aqsaali7 <aaqsa5411@gmail.com>
2025-02-07 10:42:15 +0500

    updated

--- src/app/components/Features.tsx
+++ src/app/components/Features.tsx
@@ -20,7 +20,7 @@ export default function Features() {
                   alt="Pizza"
                   width={825}
                   height={433}
-                  layout="intrinsic" // This ensures the image retains its aspect ratio
+                
                 />
                 <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-0xl">
                   {" "}
@@ -40,7 +40,7 @@ export default function Features() {
                   alt="Pizza"
                   width={825}
                   height={433}
-                  layout="intrinsic" // Ensures the image retains its aspect ratio
+              
                 />
                 <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-0xl">
                   {" "}
@@ -59,7 +59,7 @@ export default function Features() {
                     alt="Cheese Pizza"
                     width={823}
                     height={433}
-                    layout="intrinsic" // Ensures the image retains its aspect ratio
+                    
                   />
                   <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-0xl">
                     {" "}
@@ -76,7 +76,7 @@ export default function Features() {
                     alt="Pizza"
                     width={823}
                     height={433}
-                    layout="intrinsic" // Ensures the image retains its aspect ratio
+                    
                   />
                   <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-0xl">
                     {" "}
@@ -92,14 +92,12 @@ export default function Features() {
                 className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 flex-grow"
               >
                 <Image
-
-
                 width={433}
                 height={433}
                   src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4ZvhLoBq-TYkVlBlbGzhT0xS2eagmhC4-wg&s"
                   className=" w-full h-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
                   alt="Pizza"
-                layout="intrinsic" 
+                 
                 
                 />
                 <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-0xl">
